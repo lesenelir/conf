0264af9fbfe77565a4bcb9e888dbc282a617ef9a
lesenelir <hi@lesenelir.me>
2023-10-16 20:55:16 +0800

    feat: setup utils package

---
+++ packages/utils/rollup.config.js
@@ -0,0 +1,12 @@
+import autoExternal from "rollup-plugin-auto-external"
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "es",
+    sourcemap: true
+  },
+  plugins: [autoExternal(), typescript({exclude: ['templates/**']})]
+}
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,3 @@
+import {sayHi} from './utils'
+
+console.log(sayHi('TypeScript'))
