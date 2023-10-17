2c646f3b4ec269545f4d34d0baa056ce6cde43a7
lesenelir <hi@lesenelir.me>
2023-10-17 16:18:42 +0800

    feat: add ui library

---
+++ packages/ui/rollup.config.js
@@ -0,0 +1,10 @@
+
+
+export default {
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "esm",
+    sourcemap: true
+  }
+}
---
+++ packages/ui/src/dialog/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../index.css'
+
+interface IProps {
+
+}
+
+function Dialog(props: IProps) {
+  return (
+    <>
+      <div className={'bg-blue-200'}>Dialog</div>
+    </>
+  )
+}
+
+export default Dialog
